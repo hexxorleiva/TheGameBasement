14eccb175e66204850ea81de626730cadd747b10
Hexxor <hexxorleiva@gmail.com>
2011-06-21 16:53:40 -0400

    Style and PHP scripts for http://thegamebasement.com up-to-date as of June 21st, 2011.

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyten_header_image_width', 1000 ) );
+define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyten_header_image_height', 198 ) );
+?>
